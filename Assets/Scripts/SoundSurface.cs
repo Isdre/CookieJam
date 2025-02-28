a1c039aaecef09aff669921dcd6e66d7fafbbc3b
Kosmik <malpa.w.klapkach@gmail.com>
2025-02-28 23:48:29 +0100

    Add steps sounds to Player

---
+++ Assets/Scripts/SoundSurface.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SoundSurface : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] sounds;
+
+    public AudioClip GetSound()
+    {
+        int index = Random.Range(0, sounds.Length);
+        return sounds[index];
+    }
+}
---
+++ Assets/Scripts/StepsSounds.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class StepsSounds : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private float stepLength = 0.2f;
+    [SerializeField]
+    private AudioClip defaultStepSound;
+    [SerializeField]
+    private LayerMask steppableLayers;
+
+    private Vector3 previousPosition;
+
+    private bool isOnFloor;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        previousPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        var position = transform.position;
+
+        var wasOnFloor = isOnFloor;
+        isOnFloor = Physics.Raycast(position + Vector3.up * 0.5f, Vector3.down, out var hitInfo, 1, steppableLayers);
+        if (isOnFloor)
+        {
+            if (wasOnFloor == false || (position - previousPosition).sqrMagnitude > stepLength * stepLength)
+            {
+                PlayStepSound(hitInfo.collider.gameObject);
+                previousPosition = position;
+            }
+        }
+    }
+
+    private void PlayStepSound(GameObject steppedObject)
+    {
+        var sound = GetStepSound(steppedObject);
+        audioSource.PlayOneShot(sound);
+    }
+
+    private AudioClip GetStepSound(GameObject steppedObject) => steppedObject.TryGetComponent<SoundSurface>(out var surface)
+        ? surface.GetSound()
+        : defaultStepSound;
+}
