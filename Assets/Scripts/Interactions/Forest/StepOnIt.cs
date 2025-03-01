729af8f5a823a4e8f8401c5e874a2e07c888e8bc
Gilbert Guszcza <guszczagilbert@gmail.com>
2025-03-01 13:05:27 +0100

    add scrpits

---
+++ Assets/Scripts/Interactions/Forest/StepOnIt.cs
@@ -0,0 +1,13 @@
+using Bipolar.InteractionSystem;
+using UnityEngine;
+
+namespace Forest {
+    public class StepOnIt : Interaction
+    {
+
+        public override void Interact(Interactor interactor)
+        {
+            
+        }
+    }
+}
