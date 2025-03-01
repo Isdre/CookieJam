3ccbb6aec631ca78af37e4b6e85455398a11ac7d
Kosmik <malpa.w.klapkach@gmail.com>
2025-03-02 00:49:51 +0100

    Hamster story

---
+++ Assets/Scripts/Interactions/RollDeathSequence.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class RollDeathSequence : DeathSequence
+{
+    [SerializeField]
+    private float sequenceDuration = 2;
+
+    [Header("Rolling")]
+    [SerializeField]
+    private Transform rotatingBody;
+    [SerializeField]
+    private float rotationSpeed = 10;
+
+    [Header("Bleeding")]
+    [SerializeField]
+    private MeshRenderer bodyRenderer;
+    [SerializeField]
+    private float colorizingDuration = 1.5f;
+    [SerializeField]
+    private Color bloodyColor;
+    [SerializeField]
+    private ParticleSystem bloodParticles;
+
+    private bool isRotating;
+
+    public override void Play()
+    {
+        Invoke(nameof(Completed), sequenceDuration);
+        isRotating = true;
+        bloodParticles?.Play();
+        bodyRenderer.material.DOColor(bloodyColor, colorizingDuration); 
+    }
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            rotatingBody.Rotate(rotatingBody.forward, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
