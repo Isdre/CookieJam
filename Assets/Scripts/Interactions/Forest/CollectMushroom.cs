ff5ec26ad81e0b2e2faed43d5d4e40da66322ae1
Beata0020 <b.orlinska05@gmail.com>
2025-03-02 10:40:38 +0100

    audio mixer

--- Assets/Scripts/Interactions/Forest/CollectMushroom.cs
+++ Assets/Scripts/Interactions/Forest/CollectMushroom.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using Bipolar.InteractionSystem;
+using UnityEngine.Events;
 
 namespace Forest {
 public class CollectMushroom : Interaction {
+        public UnityEvent OnInteract;
         [SerializeField]
         private Transform mushroom;
         public override void Interact(Interactor interactor) {
             MushroomBasket.instance.AddMushroom();
             Destroy(mushroom.gameObject);
+            OnInteract.Invoke();
         }
     }
 }
